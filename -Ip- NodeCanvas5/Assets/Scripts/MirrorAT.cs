24b76f567f51a91687b99e05262e135ce5bc757a
moonicecreamstudio <moonicecreamstudio@gmail.com>
2025-02-07 12:53:48 -0500

    Added lessons, and Mirror

---
+++ -Ip- NodeCanvas5/Assets/Scripts/MirrorAT.cs
@@ -0,0 +1,48 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class MirrorAT : ActionTask {
+
+        public BBParameter<Vector3> targetPosition;
+        public Transform targetTransform;
+		public Vector3 mirrorPosition;
+
+        //public Transform playerPosition;
+		//public float offset;
+		
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+
+            //agent.transform.position = new Vector3 (playerPosition.position.x, playerPosition.position.y, -playerPosition.position.z);
+
+            targetPosition.value = (mirrorPosition + new Vector3 (targetTransform.position.x, targetTransform.position.y, -targetTransform.position.z));
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
